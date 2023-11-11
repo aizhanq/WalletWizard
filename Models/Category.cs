f26e6a5235d2fe10a1f0ae0593827bfcc9e78c46
Aizhan <aizhankarasartova@gmail.com>
2023-11-11 15:09:12 +0600

    add models, add migration

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wallet_Wizard.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wallet_Wizard.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransationId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
