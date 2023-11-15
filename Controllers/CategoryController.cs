a57df0a3248b183ffb1499a182ae3fa6fcc13e3c
future <aizhankarasartova@gmail.com>
2023-11-15 21:13:01 +0600

    edit category function

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -44,18 +44,21 @@ namespace Wallet_Wizard.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
-        public IActionResult Create()
+        // GET: Category/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
         {
-            return View(new Category());
+            if(id == 0) 
+                return View(new Category());
+            else
+                return View(_context.Categories.Find(id));
         }
 
-        // POST: Category/Create
+        // POST: Category/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
@@ -65,58 +68,7 @@ namespace Wallet_Wizard.Controllers
             }
             return View(category);
         }
-
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
-        {
-            if (id != category.CategoryId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.CategoryId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
+    
         // GET: Category/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
