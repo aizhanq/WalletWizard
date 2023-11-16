7e2866a2ef769f827cced94286fcb464e0f8fb3c
aizhan <aizhankarasartova@gmail.com>
2023-11-16 15:49:49 +0600

    add edit and delete icons, delete confirmation

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -26,24 +26,6 @@ namespace Wallet_Wizard.Controllers
                         Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -69,24 +51,6 @@ namespace Wallet_Wizard.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
-        }
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
             return View(category);
         }
 
